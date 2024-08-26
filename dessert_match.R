ce67accfa572459302288cf60a6140cd361e948f
takeenshamloo <takeenshamloo@gmail.com>
2024-08-26 14:47:00 -0700

    What is your favorite dessert

--- dessert_match.R
+++ dessert_match.R
@@ -18,3 +18,9 @@ dessert_listing <- dessert_elements %>%
   head(.,-3) %>%               # 3 last ones were not desserts 
   rowid_to_column("rank") %>%  # adding a column using the row number as a proxy for the rank
   write_csv("data/iconic_desserts.csv") # save it as csv
+
+if (any(str_detect(dessert_listing, "salsa"))){
+  print("Theres some kind of cookie")
+} else{
+  print("No dice...")
+}
